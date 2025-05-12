6f6794fbc705b960bb8980994fb2bee43ccb52de
mtulley <mftulley@gmail.com>
2025-05-12 16:54:56 -0700

    Revert "testing github"
    This reverts commit d272e673df18ad8eefa371359ed2b21e2f33174d.

--- testing_trashme.py
+++ testing_trashme.py
@@ -1,1 +1,14 @@
-yolo
+import numpy as np
+
+x_ref = np.array([1,2,3])
+y_ref = np.array([4,5,6])
+x_src = np.array([7,8,9])
+y_src = np.array([10,11,12])
+
+ref_points = np.vstack((x_ref, y_ref)).T
+
+R = np.eye(2)
+t = np.zeros((2,1))
+print(t)
+
+
